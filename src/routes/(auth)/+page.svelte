1b70168d9ddba7b3fed527b0a973e6738ebec0ca
EEido <sakvipubp1120@gmail.com>
2023-06-26 21:36:59 +0700

    add new pages

---
+++ src/routes/(auth)/+page.svelte
@@ -0,0 +1,20 @@
+
+<div class="h-screen flex flex-col items-center justify-center">
+    <div> 
+        <label class="label">
+            <span>Email</span>
+            <input class="input" title="Input (text)" type="text" placeholder="" />
+        </label>
+        <label class="label">
+            <span>Password</span>
+            <input class="input" title="Input (text)" type="text" placeholder="" />
+        </label>
+    </div>
+    <div>
+        <a href="/stores"><button type="button" class="btn-block btn variant-filled-surface my-8">Login </button></a>
+        <a href="/register"> or Register</a>
+    </div>
+    <div>
+        <p>Click <a href="/registerForOwners"> HERE </a> if you want to come sell with us</p>
+    </div>
+</div>
