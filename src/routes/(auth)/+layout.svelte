2a8086fdfd235cfbdd54a78ce4f21ddcc4c33e74
EEido <sakvipubp1120@gmail.com>
2023-06-26 22:37:38 +0700

    pages changes

--- src/routes/(auth)/+layout.svelte
+++ src/routes/(auth)/+layout.svelte
@@ -1,19 +1,19 @@
 <script>
-import '../../theme.postcss';
-import '@skeletonlabs/skeleton/styles/skeleton.css';
-import '../../app.postcss';
-import { AppShell, AppBar } from '@skeletonlabs/skeleton';
+    import '../../theme.postcss';
+    import '@skeletonlabs/skeleton/styles/skeleton.css';
+    import '../../app.postcss';
+    import { AppShell, AppBar } from '@skeletonlabs/skeleton';
 </script>
 <AppShell>
-	<!-- (header) -->
-	<!-- (sidebarLeft) -->
-	<svelte:fragment slot="sidebarRight">
-		<img src="../src/lib/images/Logo.png" width="950px" alt="Logo">
-	</svelte:fragment>
-	<!-- (pageHeader) -->
-	<!-- Router Slot -->
-	<slot />
-	<!-- ---- / ---- -->
-	<!-- (pageFooter) -->
-	<!-- (footer) -->
+    <!-- (header) -->
+    <!-- (sidebarLeft) -->
+    <svelte:fragment slot="sidebarRight">
+        <img src="../src/lib/images/Logo.png" width="950px" alt="Logo">
+    </svelte:fragment>
+    <!-- (pageHeader) -->
+    <!-- Router Slot -->
+    <slot />
+    <!-- ---- / ---- -->
+    <!-- (pageFooter) -->
+    <!-- (footer) -->
 </AppShell>
