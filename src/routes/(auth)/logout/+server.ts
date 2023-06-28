5db17be9491a2125ffd3ae516ab100feea420aa3
vinchhay sea <seavinchhay00@gmail.com>
2023-06-28 14:20:03 +0700

    Shop endpoint started

--- src/routes/(auth)/logout/+server.ts
+++ src/routes/(auth)/logout/+server.ts
@@ -2,10 +2,10 @@ import { error, redirect } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
 
 export const POST: RequestHandler = async ({ locals }) => {
-  const { error: err } = await locals.supabase.auth.signOut();
-  console.log(locals.getSession());
-  if (err) {
-    throw error(500, err.message);
-  }
-  throw redirect(303, '/login');
+	const { error: err } = await locals.supabase.auth.signOut();
+	console.log(locals.getSession());
+	if (err) {
+		throw error(500, err.message);
+	}
+	throw redirect(303, '/login');
 };
